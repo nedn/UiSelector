9f341d557f0e0020fba5073d4da83126332e728a
nednguyen <nednguyen@google.com>
2015-02-22 18:38:36 -0800

    Add libnanothhtpd

--- src/selector/SelectorHttpServer.java
+++ src/selector/SelectorHttpServer.java
@@ -1,5 +1,10 @@
 package selector;
+import fi.iki.elonen.NanoHTTPD;
 
-public class SelectorHttpServer {
+public class SelectorHttpServer extends NanoHTTPD {
+
+	public SelectorHttpServer(int port) {
+		super(port);
+	}
 
 }
