f2f2aaec007a0c1add979f59882802f606461d89
nednguyen <nednguyen@google.com>
2015-02-22 17:08:56 -0800

    Initial commit

---
+++ src/selector/Runner.java
@@ -0,0 +1,8 @@
+package selector;
+import com.android.uiautomator.testrunner.UiAutomatorTestCase;
+
+public class Runner extends UiAutomatorTestCase {
+	public void testRun() {
+		System.out.println("Hello");
+	}
+}
---
+++ src/selector/SelectorHttpServer.java
@@ -0,0 +1,5 @@
+package selector;
+
+public class SelectorHttpServer {
+
+}
