cc2188be46d6fdfc61340960c8cbfb613fd878f1
nednguyen <nednguyen@google.com>
2015-02-24 22:01:34 -0800

    update

--- src/selector/Runner.java
+++ src/selector/Runner.java
@@ -1,8 +1,35 @@
 package selector;
+
 import com.android.uiautomator.testrunner.UiAutomatorTestCase;
 
 public class Runner extends UiAutomatorTestCase {
-	public void testRun() {
-		System.out.println("Hello");
+	public static final int DEFAULT_PORT = 9008;
+	SelectorHttpServer server;
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		String portStringValue = getParams().getString("PORT");
+		int port = DEFAULT_PORT;
+		if (!portStringValue.isEmpty()) {
+			port = Integer.parseInt(portStringValue);
+		}
+		server = new SelectorHttpServer(port);
+		server.start();
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		server.stop();
+		server = null;
+		super.tearDown();
+	}
+
+	public void testRun() throws InterruptedException {
+		while (server.isAlive()) {
+			synchronized (server.waitLock) {
+				server.waitLock.wait();
+			}
+		}
 	}
 }
--- src/selector/SelectorHttpServer.java
+++ src/selector/SelectorHttpServer.java
@@ -1,10 +1,88 @@
 package selector;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.graphics.Rect;
+
+import com.android.uiautomator.core.UiObject;
+import com.android.uiautomator.core.UiObjectNotFoundException;
+import com.android.uiautomator.core.UiSelector;
+
 import fi.iki.elonen.NanoHTTPD;
 
 public class SelectorHttpServer extends NanoHTTPD {
+	
+	public Object waitLock = new Object();
+	
+	public static final String CLASS_NAME = "class_name";
+	public static final String RESOURCE_ID = "resource_id";
+
 
 	public SelectorHttpServer(int port) {
 		super(port);
-	}
+	} 
+	
+	public static final Response UNSUPPORTED_ERROR = new Response(
+			Response.Status.BAD_REQUEST, MIME_PLAINTEXT, "Unsuported protocol.");
+	
+	public static final Response UI_OBJECT_NOT_FOUND_ERROR = new Response(
+			Response.Status.NOT_FOUND, MIME_PLAINTEXT, "Ui object not found.");
 
+	public static final Response JSON_PROCESSING_ERROR = new Response(
+			Response.Status.INTERNAL_ERROR, MIME_PLAINTEXT, "Json processing error.");
+
+	
+	Response GetResponseFromSelectedUiObject(UiObject object) {
+		JSONObject responseObject = new JSONObject();
+		try {
+			Rect bound = object.getBounds();
+			responseObject.put("top", bound.top);
+			responseObject.put("bottom", bound.bottom);
+			responseObject.put("left", bound.left);
+			responseObject.put("right", bound.right);
+			responseObject.put("isClickable", object.isClickable());
+			responseObject.put("isScrollable", object.isScrollable());
+		} catch (JSONException e) {
+			return JSON_PROCESSING_ERROR;
+		} catch (UiObjectNotFoundException e) {
+			return UI_OBJECT_NOT_FOUND_ERROR;
+		}
+		return new Response(responseObject.toString());
+	}
+	
+	@Override
+	public Response serve(IHTTPSession session) {
+		if ("/stop".equals(session.getUri())) {
+			stop();
+			synchronized (waitLock) {
+				waitLock.notify();
+			}
+			return new Response("Server stopped.");
+		} else if ("GET".equals(session.getMethod())){
+			Map<String, String> parms = session.getParms();
+			if (parms.size() != 1) {
+				return UNSUPPORTED_ERROR;
+			}
+			Map.Entry<String, String> entry = 
+					parms.entrySet().iterator().next();
+			String selectorType = entry.getKey();
+			String selectorValue = entry.getValue();
+			UiObject object = null; 
+			switch (selectorType) {
+			case CLASS_NAME:
+				object = new UiObject(new UiSelector().className(selectorValue));
+				break;
+			case RESOURCE_ID:
+				object = new UiObject(new UiSelector().resourceId(selectorValue));
+				break;
+			default:
+				return UNSUPPORTED_ERROR;
+			}
+			return GetResponseFromSelectedUiObject(object);
+		} else {
+			return UNSUPPORTED_ERROR;
+		}
+	}
 }
