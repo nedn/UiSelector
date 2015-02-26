874324704a87ceede23f42d97782477430b687c7
nednguyen@google.com <nednguyen@google.com>
2015-02-26 13:43:23 -0800

    Update

--- request.py
+++ request.py
@@ -5,15 +5,17 @@ def stop():
   conn = httplib.HTTPConnection('localhost', 9008)
   conn.request('GET', '/stop')
 
-def SelectCorpusBar():
+def SelectChromeMenu(print_results=False):
   conn = httplib.HTTPConnection('localhost', 9008)
   HEADERS = {
-  'resource_id' : 'com.google.android.googlequicksearchbox:id/corpus_bar'
+      'resource_id' : 'com.android.chrome:id/document_menu_button'
   }
   conn.request('GET', '/select', headers=HEADERS)
   res = conn.getresponse()
   data = res.read()
+  if print_results:
+    print data
 
 if __name__ == '__main__':
   for _ in range(100):
-    SelectCorpusBar()
+    SelectChromeMenu()
